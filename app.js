4f76c2714edf5f165929ce904e38df1e3d50ef0c
Apiwat kalapat <gamevongola6@gmail.com>
2020-07-05 19:49:52 +0700

    game

--- app.js
+++ app.js
@@ -23,25 +23,193 @@
 //   res.sendFile(__dirname + '/index.html');
 // });
 
+//---------------------------------------------------------------------------------------------------------------------------//
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/farm1', (req, res) => res.sendFile(__dirname + '/index.html'))
-app.get('/farm2', (req, res) => res.sendFile(__dirname + '/index3.html'))
-app.get('/farm3', (req, res) => res.sendFile(__dirname + '/index4.html'))
-app.get('/farm4', (req, res) => res.sendFile(__dirname + '/index5.html'))
+app.use('/images', express.static('images'));
+
+app.get('/farm1', (req, res) =>
+  res.sendFile(__dirname + '/index.html')
+)
+app.get('/farm2', (req, res) =>
+  res.sendFile(__dirname + '/index3.html')
+)
+app.get('/farm3', (req, res) =>
+  res.sendFile(__dirname + '/index4.html')
+)
+app.get('/farm4', (req, res) =>
+  res.sendFile(__dirname + '/index5.html')
+)
+
+app.get('/moveleft/:id', (req, res) =>{
+  const onvif = require('node-onvif');
+
+  let device = new onvif.OnvifDevice({
+    xaddr: `http://${req.params.id}/onvif/device_service`,
+    user: 'admin',
+    pass: 'ciimostoiot95'
+  });
+
+  device.init().then(() => {
+
+    let params = {
+      'speed': { x: -1.0, y: 0.0, z: 0.0 },
+      'timeout': 60
+    };
+
+    device.ptzMove(params).then(() => {
+      console.log('Succeeded to move.');
+
+      setTimeout(() => {
+        device.ptzStop().then(() => {
+          console.log('Succeeded to stop.');
+        }).catch((error) => {
+          console.error(error);
+        });
+      }, 1500);
+    }).catch((error) => {
+      console.error(error);
+    });
+
+  }).then(() => {
+    console.log('Done!');
+  }).catch((error) => {
+    console.error(error);
+  });
+
+  res.end()
+})
+
+
+app.get('/moveright/:id', (req, res) =>{
+  const onvif = require('node-onvif');
+
+  let device = new onvif.OnvifDevice({
+    xaddr: `http://${req.params.id}/onvif/device_service`,
+    user: 'admin',
+    pass: 'ciimostoiot95'
+  });
+
+  device.init().then(() => {
+
+    let params = {
+      'speed': { x: 1.0, y: 0.0, z: 0.0 },
+      'timeout': 60
+    };
+
+    device.ptzMove(params).then(() => {
+      console.log('Succeeded to move.');
+
+      setTimeout(() => {
+        device.ptzStop().then(() => {
+          console.log('Succeeded to stop.');
+        }).catch((error) => {
+          console.error(error);
+        });
+      }, 1500);
+    }).catch((error) => {
+      console.error(error);
+    });
+
+  }).then(() => {
+    console.log('Done!');
+  }).catch((error) => {
+    console.error(error);
+  });
+
+  res.end()
+})
+
+app.get('/movehight/:id', (req, res) =>{
+  const onvif = require('node-onvif');
+
+  let device = new onvif.OnvifDevice({
+    xaddr: `http://${req.params.id}/onvif/device_service`,
+    user: 'admin',
+    pass: 'ciimostoiot95'
+  });
+
+  device.init().then(() => {
+
+    let params = {
+      'speed': { x: 0.0, y: 1.0, z: 0.0 },
+      'timeout': 60
+    };
+
+    device.ptzMove(params).then(() => {
+      console.log('Succeeded to move.');
+
+      setTimeout(() => {
+        device.ptzStop().then(() => {
+          console.log('Succeeded to stop.');
+        }).catch((error) => {
+          console.error(error);
+        });
+      }, 1500);
+    }).catch((error) => {
+      console.error(error);
+    });
+
+  }).then(() => {
+    console.log('Done!');
+  }).catch((error) => {
+    console.error(error);
+  });
+
+  res.end()
+})
+
+app.get('/movelow/:id', (req, res) =>{
+  const onvif = require('node-onvif');
+
+  let device = new onvif.OnvifDevice({
+    xaddr: `http://${req.params.id}/onvif/device_service`,
+    user: 'admin',
+    pass: 'ciimostoiot95'
+  });
+
+  device.init().then(() => {
+
+    let params = {
+      'speed': { x: 0.0, y: -1.0, z: 0.0 },
+      'timeout': 60
+    };
+
+    device.ptzMove(params).then(() => {
+      console.log('Succeeded to move.');
+
+      setTimeout(() => {
+        device.ptzStop().then(() => {
+          console.log('Succeeded to stop.');
+        }).catch((error) => {
+          console.error(error);
+        });
+      }, 1500);
+    }).catch((error) => {
+      console.error(error);
+    });
+
+  }).then(() => {
+    console.log('Done!');
+  }).catch((error) => {
+    console.error(error);
+  });
+
+  res.end()
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
 Stream = require('node-rtsp-stream')
 stream = new Stream({
   name: 'name',
-  streamUrl: 'rtsp://guest:Passw0rd@192.168.3.11:554/cam/realmonitor?channel=14&subtype=0',
-  wsPort: 9000,
-  ffmpegOptions: { // options ffmpeg flags
-    '-stats': '', // an option with no neccessary value uses a blank string
-    '-r': 30 // options with required values specify the value after the key
+  streamUrl: 'rtsp://guest:Passw0rd@192.168.3.11:554/cam/realmonitor?channel=1&subtype=0',
+  wsPort: 9010,
+  ffmpegOptions: { 
+    '-stats': '', 
+    '-r': 30 
   }
 });
 
@@ -50,9 +218,9 @@ Stream2 = new Stream({
   name: 'name',
   streamUrl: 'rtsp://guest:Passw0rd@192.168.3.11:554/cam/realmonitor?channel=9&subtype=0',
   wsPort: 9001,
-  ffmpegOptions: { // options ffmpeg flags
-    '-stats': '', // an option with no neccessary value uses a blank string
-    '-r': 30 // options with required values specify the value after the key
+  ffmpegOptions: { 
+    '-stats': '', 
+    '-r': 30 
   }
 });
 
@@ -61,9 +229,9 @@ Stream3 = new Stream({
   name: 'name',
   streamUrl: 'rtsp://guest:Passw0rd@192.168.3.11:554/cam/realmonitor?channel=5&subtype=0',
   wsPort: 9002,
-  ffmpegOptions: { // options ffmpeg flags
-    '-stats': '', // an option with no neccessary value uses a blank string
-    '-r': 30 // options with required values specify the value after the key
+  ffmpegOptions: { 
+    '-stats': '', 
+    '-r': 30
   }
 });
 
@@ -72,11 +240,12 @@ Stream4 = new Stream({
   name: 'name',
   streamUrl: 'rtsp://guest:Passw0rd@192.168.3.11:554/cam/realmonitor?channel=13&subtype=0',
   wsPort: 9003,
-  ffmpegOptions: { // options ffmpeg flags
-    '-stats': '', // an option with no neccessary value uses a blank string
-    '-r': 30 // options with required values specify the value after the key
+  ffmpegOptions: { 
+    '-stats': '', 
+    '-r': 30 
   }
 });
+//------------------------------------------------------------------------------------------------------------//
 
 // const Stream = require('videoStream')
 
@@ -112,19 +281,57 @@ Stream4 = new Stream({
 //     console.error(error);
 // });
 
+//----------------------------------------------------move camera start---------------------------------//
 // const onvif = require('node-onvif');
 
-// console.log('Start the discovery process.');
-// // Find the ONVIF network cameras.
-// // It will take about 3 seconds.
-// onvif.startProbe().then((device_info_list) => {
-//   console.log(device_info_list.length + ' devices were found.');
-//   // Show the device name and the URL of the end point.
-//   device_info_list.forEach((info) => {
-//     console.log('- ' + info.urn);
-//     console.log('  - ' + info.name);
-//     console.log('  - ' + info.xaddrs[0]);
+// let device = new onvif.OnvifDevice({
+//   xaddr: 'http://192.168.3.83:10080/onvif/device_service',
+//   user : 'admin',
+//   pass : 'ciimostoiot95'
+// });
+
+// device.init().then(() => {
+
+//   let params = {
+//     'speed': { x: 1.0, y: 0.0, z: 0.0 },
+//     'timeout': 60 
+//   };
+
+//   device.ptzMove(params).then(() => {
+//     console.log('Succeeded to move.');
+
+//     setTimeout(() => {
+//       device.ptzStop().then(() => {
+//         console.log('Succeeded to stop.');
+//       }).catch((error) => {
+//         console.error(error);
+//       });
+//     }, 1500);
+//   }).catch((error) => {
+//     console.error(error);
 //   });
+
+// }).then(() => {
+//   console.log('Done!');
+// }).catch((error) => {
+//   console.error(error);
+// });
+//----------------------------------------------------move camera end---------------------------------//
+
+
+
+// device.init().then(() => {
+//   // Move the camera
+//   device.ptzMove({
+//     'speed': {
+//       x: -0.5, // Speed of pan (in the range of -1.0 to 1.0)
+//       y: 0.0, // Speed of tilt (in the range of -1.0 to 1.0)
+//       z: 0.0  // Speed of zoom (in the range of -1.0 to 1.0)
+//     },
+//     'timeout': 1 // seconds
+//   });
+// }).then(() => {
+//   console.log('Done!');
 // }).catch((error) => {
 //   console.error(error);
 // });
